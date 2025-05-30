e6c9c957dbc0f55cf632adf6537c30ce78487780
mariyadyshkant <mariya.dyshkant@gmail.com>
2025-05-30 14:55:27 +0200

    Add email invitation list and user email prompt functionality

--- js-mail/script.js
+++ js-mail/script.js
@@ -1,1 +1,18 @@
 // Crea una lista di email di invitati ad una festa. Chiedi all’utente la sua email, controlla che sia nella lista di chi può accedere, stampa un messaggio appropriato sull’esito del controllo, utilizzando un ciclo for. 
+
+// Abbiamo una lista di invitati alla festa.
+const invited = [
+    "marco.rossi@gmail.com",        // 0
+    "sara.iacovazzi_35@gmail.com",  //1
+    "giulia.rossi93@gmail.com",     //2
+    "marco.bianchi@libero.it",      //3
+    "sara.verdi@alice.it",          //4
+    "luca.neri@alice.it",           //5
+    "francesca.russo@outlook.it",   //6
+    "andrea.conti@outlook.it",      //7
+    "elena.mancini@outlook.com"     //8
+]
+
+//Devo chiedere all'utente la sua mail tramite un prompt e salvo il risultato in una variabile.
+const userMail = prompt("Inserisci qui la tua email.");
+
