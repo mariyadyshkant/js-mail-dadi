c05c8a1c32ff63e7f98b053e971a25cac9c7e8b2
mariyadyshkant <mariya.dyshkant@gmail.com>
2025-05-30 16:32:51 +0200

    Implement random number generation and winner determination for player and computer

--- js-dadi/script.js
+++ js-dadi/script.js
@@ -1,1 +1,18 @@
 // Generare un numero random da 1 a 6, sia per il giocatore sia per il computer. Stabilire il vincitore, in base a chi fa il punteggio più alto.
+
+const userNumber = Math.floor(Math.random() * 7) + 1;
+const computerNumber = Math.floor(Math.random() * 7) + 1;
+
+
+// Creo un ciclo for in modo da far generare un numero di volte che stabilisco io, due numeri random, compresi tra 1 e 6. Una volta generati stabilire anche il vincitore in base al numero più alto.
+
+for (i = 1; i <= 1; i++) {
+    console.log(userNumber, computerNumber);
+    if (userNumber > computerNumber) {
+        console.log("Complimenti! Hai vinto, giocatore!");
+
+    } else {
+        console.log("Questa volta ha vinto il computer!");
+
+    }
+}
