1dcee55b60e08b03f3756bdf134152db825e0d22
mariyadyshkant <mariya.dyshkant@gmail.com>
2025-05-30 15:23:42 +0200

    Refactor invited email list for improved readability and streamline conditional logic

--- js-mail/script.js
+++ js-mail/script.js
@@ -1,18 +1,10 @@
 // Crea una lista di email di invitati ad una festa. Chiedi all’utente la sua email, controlla che sia nella lista di chi può accedere, stampa un messaggio appropriato sull’esito del controllo, utilizzando un ciclo for. 
 
 // Abbiamo una lista di invitati alla festa.
-const invited = ["marco.rossi@gmail.com",        // 0
-    "sara.iacovazzi_35@gmail.com",  //1
-    "giulia.rossi93@gmail.com",     //2
-    "marco.bianchi@libero.it",      //3
-    "sara.verdi@alice.it",          //4
-    "luca.neri@alice.it",           //5
-    "francesca.russo@outlook.it",   //6
-    "andrea.conti@outlook.it",      //7
-    "elena.mancini@outlook.com"]     //8
-
+const invited = ["marco.rossi@gmail.com", "sara.iacovazzi_35@gmail.com", "giulia.rossi93@gmail.com", "marco.bianchi@libero.it", "sara.verdi@alice.it", "luca.neri@alice.it", "francesca.russo@outlook.it", "andrea.conti@outlook.it"]
 
 console.log(invited.length);
+
 
 //Devo chiedere all'utente la sua mail tramite un prompt e salvo il risultato in una variabile.
 const userMail = prompt("Inserisci qui la tua email.");
@@ -24,8 +16,7 @@ for (let i = 0; i < invited.length; i++) {
     if (userMail === thisMail) {
         console.log(userMail, "è presente nella lista degli invitati.");
         break;
-    }
-    else {
+    } else {
         console.log(userMail, "non è presente nella lista degli invitati");
 
     }
